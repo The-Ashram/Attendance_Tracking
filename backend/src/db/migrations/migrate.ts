ce0470232d8a13f125d9fc51eaf839344dfe7b66
APANA49 <aungphonenaing57@gmail.com>
2024-09-30 00:59:42 +0800

    Adds Migrations, Logging, scripts
    Migrations for drizzle added,
    Logging functions added,
    scripts modified.

---
+++ backend/src/db/migrations/migrate.ts
@@ -0,0 +1,28 @@
+import config from "config/config"
+import { migrate } from "drizzle-orm/postgres-js/migrator"
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import log from "./../../config/log.config"
+
+const NAMESPACE = 'Migrate';
+
+const doMigrate = async () => {
+    try {
+        const connectionString = `postgres://${config.drizzle.user}:${config.drizzle.pass}@${config.drizzle.host}:${config.server.port}/${config.drizzle.database}`;
+        console.log('migrate string: ', connectionString);
+        const attendanceTrackingPG = postgres(connectionString, { max: 1 })
+        const db = drizzle(attendanceTrackingPG)
+
+        await migrate(db, {
+            migrationsFolder: "./src/db/migrations"
+        });
+        console.log("Migrations successful!");
+
+        return process.exit(0);
+    } catch (e) {
+        log.error(NAMESPACE, "Migration error: ", e);
+        return process.exit(0);
+    }
+};
+
+doMigrate();
